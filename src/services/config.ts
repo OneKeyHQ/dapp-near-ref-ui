97849670637b869a71c07df1d1d109543597cb7d
Joe <joe@ref.finance>
2022-01-17 11:39:19 +0800

    update TOTAL_PLATFORM_FEE_REVENUE value

--- src/services/config.ts
+++ src/services/config.ts
@@ -36,7 +36,7 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
           '6b175474e89094c44da98b954eedeac495271d0f.factory.bridge.near': 2,
         },
         TOTAL_PLATFORM_FEE_REVENUE:
-          process.env.TOTAL_PLATFORM_FEE_REVENUE || '242,633.0475',
+          process.env.TOTAL_PLATFORM_FEE_REVENUE || '485,266.095',
       };
     case 'development':
     case 'testnet':
@@ -75,7 +75,7 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
           'dai.fakes.testnet': 2,
         },
         TOTAL_PLATFORM_FEE_REVENUE:
-          process.env.TOTAL_PLATFORM_FEE_REVENUE || '242,633.0475',
+          process.env.TOTAL_PLATFORM_FEE_REVENUE || '485,266.095',
       };
     default:
       return {
@@ -112,7 +112,7 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
           '6b175474e89094c44da98b954eedeac495271d0f.factory.bridge.near': 2,
         },
         TOTAL_PLATFORM_FEE_REVENUE:
-          process.env.TOTAL_PLATFORM_FEE_REVENUE || '242,633.0475',
+          process.env.TOTAL_PLATFORM_FEE_REVENUE || '485,266.095',
       };
   }
 }
